e9133381a797450bef56e274cd21551cc08d9695
KNIDZE <mihaknidze@gmail.com>
2023-12-20 14:46:43 +0200

    Functional

---
+++ script.js
@@ -0,0 +1,36 @@
+function countLogiks(){
+  let logiks = +document.getElementById('logik').value;
+  let minutes = +document.getElementById('minutes').value;
+  let resultHeader = document.querySelector('h1')
+  let decadeAmount = Math.floor(minutes / 10);
+  let stavka = 0;
+  if (minutes < 5) {
+    resultHeader.innerHTML = ''+logiks;
+    return
+  }
+  if (5<=  minutes && minutes < 10){
+    console.log(5<=  minutes < 10);
+    stavka = 5;
+    console.log(logiks+ logiks*(stavka/100))
+    resultHeader.innerHTML = (logiks + logiks * (stavka /100)).toString();
+    return
+  }
+  console.log(decadeAmount)
+  if (1==decadeAmount){
+    stavka = 7;
+  }
+  if (2==decadeAmount) {
+    stavka = 9;
+  }
+  if (3 <= decadeAmount < 5){
+    stavka = 12;
+  }
+  if  (5 <= decadeAmount){
+    stavka = 15;
+  }
+  console.log(stavka / 100)
+  for (let i = 0; i < decadeAmount; i++) {
+    logiks = logiks + logiks * (stavka / 100);
+  }
+  resultHeader.innerHTML = ''+Math.floor(logiks);
+}
